76bd3ff9e1018107b14df93db89d7f21fa643bdf
ilsays <ilya.stanislavov@gmail.com>
2024-09-12 12:39:14 +0700

    переделано

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-from src.hh_api import VacanciesHH
-from src.user_usage import interface
-from src.connector import Connector
+from src.hh_api import HHVacancies
+from src.configuration import DATA_DIR
+from src.connector import ConnectorJson
+from src.user_usage import start_user_interaction
+
+VACANCIES_PATH = DATA_DIR / "vacancies.json"
+
+api_client = HHVacancies()
+connector = ConnectorJson(VACANCIES_PATH)
 
 
 def main():
-    user_input = input("Введите название вакансии: ")
-    user_request = VacanciesHH(f"../hh_vacancies/data/{user_input}_vacancies.json")
 
-    print("Ожидаем получения данных по вакансиям...")
+    """Основная работа программы"""
 
-    user_request.load_vacancies(user_input)
+    search_text = input('Введите текст для поиска вакансий: ')
 
-    print("Данные записаны")
+    print('Получаем вакансии...')
+    vacancies = api_client.get_vacancies(search_text)
 
-    connect = Connector(f"../hh_vacancies/data/{user_input}_vacancies.json")
-    connect.add_vacancy()
-    interface(f"../hh_vacancies/data/{user_input}_vacancies.json")
+    print('Сохраняем в базу')
+    for vac in vacancies:
+        connector.add_vacancy(vac)
+
+    start_user_interaction(connector)
 
 
 if __name__ == "__main__":
     main()
+
+
+
--- src/vacancies.py
+++ src/vacancies.py
@@ -1,51 +1,46 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Vacancies:
 
-    def __init__(self, name_vacancy: str,
-                 url_vacancy: str,
-                 city_vacancy: str, salary_from: int | None,
-                 salary_to: int | None,
-                 salary_currency: str | None,
-                 description_vacancy: str) -> None:
-        self.name_vacancy = name_vacancy
-        self.url_vacancy = url_vacancy
-        self.city_vacancy = city_vacancy
+    name: str
+    url: str
+    salary_currency: str = 'RUR'
+    salary_from: int | None = None
+    salary_to: int | None = None
 
-        if self._validation(salary_from) is None:
-            if salary_from is None:
-                self.salary_from = 0
-            else:
-                self.salary_from = salary_from
+    def __post_init__(self):
 
-        if self._validation(salary_to) is None:
-            if salary_to is None:
-                self.salary_to = 0
-            else:
-                self.salary_to = salary_to
-
-        self.salary_currency = salary_currency
-        self.description_vacancy = description_vacancy
+        self._validation(self.salary_from)
+        self._validation(self.salary_to)
 
     @staticmethod
-    def _validation(salary):
+    def _validation(salary: int | None) -> None:
         """
         Метод класса осуществляющий валидацию заработной платы вакансий
         """
-        if salary is not None and salary < 0:
-            raise ValueError("Salary cannot be negative")
 
-    def __str__(self):
-        return (f"""Наименование вакансии: {self.name_vacancy}
-Город, в котором расположен офис компании: {self.city_vacancy} 
-Заработная плата: от {self.salary_from} - до {self.salary_to} {self.salary_currency}
-Описание вакансии: {self.description_vacancy}""")
+        if salary is not None:
+            assert salary > 0
 
-    def __repr__(self):
-        return f"""{self.__class__.__name__}({self.name_vacancy}, {self.url_vacancy}, {self.city_vacancy},
-{self.salary_to}, {self.salary_from}, {self.salary_currency}, {self.description_vacancy}"""
-
-    def __le__(self, other):
-        if self.salary_to and other.salary_to:
-            return self.salary_to <= other.salary_to
+    def __lt__(self, other: 'Vacancies') -> bool:
 
         if self.salary_from and other.salary_from:
-            return self.salary_from <= other.salary_from
+            return self.salary_from < other.salary_from
+
+        if self.salary_to and other.salary_to:
+            return self.salary_to < other.salary_to
+
+        self_salary = self.salary_from or self.salary_to
+        other_salary = other.salary_from or other.salary_to
+        return self_salary < other_salary
+
+    def __eq__(self, other: 'Vacancies') -> bool:
+
+        eq_from = self.salary_from == other.salary_from
+        eq_to = self.salary_to == other.salary_to
+
+        return eq_from and eq_to
+
+
